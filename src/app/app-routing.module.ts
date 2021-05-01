8c95f851c7143fe62337a4a78117a992c0c56692
Dennis Chacko <dennis.v.chacko@gmail.com>
2021-05-01 18:37:31 -0500

    implement oidc with Auth0 using angular-oauth2-oidc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './landing/landing.component';
+import { AuthGuard } from './auth/auth-guard.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'landing' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'landing', component: LandingComponent },
 ];
 
