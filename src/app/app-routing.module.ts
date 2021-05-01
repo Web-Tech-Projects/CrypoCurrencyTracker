5e66098b7f4a59df70491a6f1eea712a151c02ea
Dennis Chacko <39276940+dvchacko@users.noreply.github.com>
2021-05-01 17:51:32 -0500

    Review/style bootstrap (#1)
    Style the app using bootstrap

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'landing' },
+  { path: 'home', component: HomeComponent },
+  { path: 'landing', component: LandingComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
